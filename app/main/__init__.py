4de0d1cfcd9d43d5cff9fd4e3fdb4eb7a89befbe
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-08-25 00:18:24 -0300

    CU-214hrn Wrote basic functionality for GET /extract

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,7 @@
 ## Based on https://www.freecodecamp.org/news/structuring-a-flask-restplus-web-service-for-production-builds-c2ec676de563/
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 from .config import config_by_name
 
@@ -10,5 +11,14 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
+
+    # Set up CORS
+    CORS(app, resources={r'/*': {'origins': '*'}})
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization, true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
 
     return app
--- app/main/controllers/routes.py
+++ app/main/controllers/routes.py
@@ -1,6 +1,34 @@
-from flask import request
+from flask import request, abort, jsonify
+import justext
+import requests
+
+def get_title(html):
+    temp = html.decode("utf-8", errors='ignore').split("<title", 1)[1]
+    temp = temp.split(">", 1)[1]
+    title = temp.split("</title>", 1)[0]
+    return title
 
 def set_routes(app):
     @app.route('/')
     def index():
         return '<h1>Welcome to the Knolist API!</h1>'
+
+    @app.route('/extract')
+    def extract():
+        url = request.args.get('url')
+        response = requests.get(url)
+
+        paragraphs = justext.justext(response.content, justext.get_stoplist("English"))
+        real_text = ""
+
+        for paragraph in paragraphs:
+            if not paragraph.is_boilerplate:
+                real_text += paragraph.text
+                real_text += "\n\n"
+        
+        return jsonify({
+            'success': True,
+            'content': real_text,
+            'url': url,
+            'title': get_title(response.content)
+        })
