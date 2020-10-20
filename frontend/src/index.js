6a50624e466037c9b9abb338e8f2330ef6df0c4f
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-10-20 03:35:14 -0300

    CU-2d59qj Start implementation of the header bar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Button} from 'rsuite';
+import {Navbar, Nav, Button, FlexboxGrid} from 'rsuite';
 
-import './index.css';
 // import default style
 import 'rsuite/dist/styles/rsuite-default.css';
+import './index.css';
 
-function App() {
-    return <Button>Hello World</Button>;
+import horizontalLogo from './images/horizontal_main.png';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            curProject: "Knolist"
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Header curProject={this.state.curProject}/>
+            </div>
+        );
+    }
+}
+
+function Header(props) {
+    return (
+        <Navbar>
+            <FlexboxGrid justify="space-between" align="middle">
+                <Navbar.Header>
+                    <img className="limit-height" src={horizontalLogo} alt="Knolist"/>
+                </Navbar.Header>
+                <FlexboxGrid.Item>
+                    <span id="project-title">Current Project: {props.curProject}</span>
+                </FlexboxGrid.Item>
+                <Nav pullRight>
+                    <Button appearance="primary" id="projects-sidebar-btn">Your<br/>Projects</Button>
+                </Nav>
+            </FlexboxGrid>
+        </Navbar>
+    );
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
