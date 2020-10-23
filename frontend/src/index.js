8b8da091b3232d8605998469e45de7f754f3d1e4
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-10-23 03:50:08 -0300

    CU-2f0hc9 Implement first connections with backend, localstorage to remember most recent project,

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,7 +17,8 @@ import {
     Divider,
     Whisper,
     Tooltip,
-    Modal
+    Modal,
+    Placeholder
 } from 'rsuite';
 import {Network, DataSet} from 'vis-network/standalone';
 
@@ -30,9 +31,29 @@ import horizontalLogo from './images/horizontal_main.png';
 class App extends React.Component {
     constructor(props) {
         super(props);
+        const jwt = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkZYNkFEd1BWdUJpQ3g0UjhKMWxDTCJ9.eyJpc3MiOiJodHRwczovL2tub2xpc3QudXMuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDVmNDczN2VjOWM1MTA2MDA2ZGUxNjFiYyIsImF1ZCI6Imtub2xpc3QiLCJpYXQiOjE2MDM0MTgzOTIsImV4cCI6MTYwMzUwNDc5MiwiYXpwIjoicEJ1NXVQNG1LVFFnQnR0VFcxM04wd0NWZ3N4OTBLTWkiLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImNyZWF0ZTpjb25uZWN0aW9ucyIsImNyZWF0ZTpoaWdobGlnaHRzIiwiY3JlYXRlOm5vdGVzIiwiY3JlYXRlOnByb2plY3RzIiwiY3JlYXRlOnNvdXJjZXMiLCJkZWxldGU6Y29ubmVjdGlvbnMiLCJkZWxldGU6aGlnaGxpZ2h0cyIsImRlbGV0ZTpub3RlcyIsImRlbGV0ZTpwcm9qZWN0cyIsImRlbGV0ZTpzb3VyY2VzIiwicmVhZDpwcm9qZWN0cyIsInJlYWQ6c291cmNlcyIsInJlYWQ6c291cmNlcy1kZXRhaWwiLCJzZWFyY2g6c291cmNlcyIsInVwZGF0ZTpub3RlcyIsInVwZGF0ZTpwcm9qZWN0cyIsInVwZGF0ZTpzb3VyY2VzIl19.fQcMQX4YLVrxYkFSBKmziN25yh1nh-Y7zI6JJGoShA2LKYJ1hy4WanIXkXz8t8R3etppzp5wwDLpTfa6fR1egbKx4zH5mzUPuAFVsx2EHndz9Cnb7_bokuTPDAyKf1tyQkwDZ-Ivq6pVE3OaW9fz9ve8mUzYTBNNqk-uDnv9GG8rmmobDXXFC9D1YJL_WRJ95DKvjD_TyFkMi99KUyFFbsPecyMa5NbBgVvHwfEPBfwP2WSQVxzq-mid_eLHjCTdF6hdDZqIwhkm_hw0RwCfYGAFWWomKbb3GhEiCce158fWQJMfyb9kPkkOC57BaJRx8WUXCzZjiSEJs0d7Uhgo-g";
+        const baseUrl = "https://knolist-api.herokuapp.com";
+        // const baseUrl = "http://localhost:5000"
         this.state = {
-            curProject: "Knolist",
-            showProjectsSidebar: false
+            curProject: JSON.parse(localStorage.getItem("curProject")),
+            projects: null,
+            showProjectsSidebar: false,
+            jwt: jwt,
+            baseUrl: baseUrl
+        }
+    }
+
+    getProjects = async () => {
+        const url = this.state.baseUrl + "/projects";
+        const response = await fetch(url, {
+            headers: {
+                "Authorization": "Bearer " + this.state.jwt
+            }
+        }).then(response => response.json());
+        if (response.success) {
+            return response.projects;
+        } else {
+            alert("Something went wrong!")
         }
     }
 
@@ -40,9 +61,11 @@ class App extends React.Component {
         this.setState({showProjectsSidebar: !this.state.showProjectsSidebar});
     }
 
-    setCurProject = (project) => {
-        // TODO: add callback to update graph data
-        this.setState({curProject: project})
+    setCurProject = async (projectId) => {
+        if (projectId === null) this.setState({curProject: null})
+
+        const project = this.state.projects.find(x => x.id === projectId);
+        this.setState({curProject: project});
     }
 
     projectsButton = () => {
@@ -53,16 +76,33 @@ class App extends React.Component {
         );
     }
 
+    componentDidMount() {
+        this.getProjects().then(projects => {
+            this.setState({projects: projects}, () => {
+                if (projects.length > 0 && this.state.curProject === null) {
+                    this.setState({curProject: projects[0]})
+                }
+            })
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // Update localstorage whenever the curProject changes
+        if (prevState.curProject !== this.state.curProject) {
+            localStorage.setItem("curProject", JSON.stringify(this.state.curProject));
+        }
+    }
+
     render() {
         return (
             <div>
                 <AppHeader curProject={this.state.curProject} showSidebar={this.state.showProjectsSidebar}
                            switchShowSidebar={this.switchShowProjectsSidebar}/>
                 <ProjectsSidebar show={this.state.showProjectsSidebar} curProject={this.state.curProject}
-                                 close={this.switchShowProjectsSidebar}
-                                 setCurProject={this.setCurProject}/>
+                                 jwt={this.state.jwt} baseUrl={this.state.baseUrl}
+                                 close={this.switchShowProjectsSidebar} setCurProject={this.setCurProject}/>
                 {this.projectsButton()}
-                <MindMap/>
+                <MindMap baseUrl={this.state.baseUrl} jwt={this.state.jwt} curProject={this.state.curProject}/>
             </div>
         );
     }
@@ -73,7 +113,10 @@ class MindMap extends React.Component {
         super(props);
         this.state = {
             network: null,
-            selectedNode: null
+            visNodes: null,
+            visEdges: null,
+            selectedNode: null,
+            sources: null
         }
     }
 
@@ -95,94 +138,198 @@ class MindMap extends React.Component {
         }
     }
 
-    componentDidMount() {
-        // create an array with nodes
-        const nodes = new DataSet([
-            {id: 1, label: 'Node 1'},
-            {id: 2, label: 'Node 2'},
-            {id: 3, label: 'Node 3'},
-            {id: 4, label: 'Node 4'},
-            {id: 5, label: 'Node 5'}
-        ]);
+    getSources = async (callback) => {
+        if (this.props.curProject === null) return null;
 
-        // create an array with edges
-        const edges = new DataSet([
-            {from: 1, to: 3},
-            {from: 1, to: 2},
-            {from: 2, to: 4},
-            {from: 2, to: 5}
-        ]);
+        const url = this.props.baseUrl + "/projects/" + this.props.curProject.id + "/sources";
+        const response = await fetch(url, {
+            headers: {
+                "Authorization": "Bearer " + this.props.jwt
+            }
+        }).then(response => response.json());
+        if (response.success) {
+            this.setState({sources: response.sources}, callback)
+        } else {
+            alert("Something went wrong!")
+        }
+    }
 
-        // create a network
-        const container = document.getElementById('mindmap');
-
-        // provide the data in the vis format
-        const data = {
-            nodes: nodes,
-            edges: edges
-        };
-        const options = {
-            nodes: {
-                shape: "box",
-                size: 16,
-                margin: 10,
-                physics: false,
-                chosen: false,
-                color: {
-                    // background: nodeBackgroundDefaultColor
-                },
-                widthConstraint: {
-                    maximum: 500
-                }
+    updateSourcePosition = async (sourceId, x, y) => {
+        const url = this.props.baseUrl + "/sources/" + sourceId;
+        const response = await fetch(url, {
+            method: "PATCH",
+            headers: {
+                "Authorization": "Bearer " + this.props.jwt,
+                "Content-Type": "application/json"
             },
-            edges: {
-                arrows: {
-                    to: {
-                        enabled: true
+            body: JSON.stringify({
+                "x_position": x,
+                "y_position": y
+            })
+        }).then(response => response.json());
+        if (!response.success) {
+            alert("Something went wrong!");
+        }
+    }
+
+    /* Helper function to generate position for nodes
+    This function adds an offset to  the randomly generated position based on the
+    position of the node's parent (if it has one)
+     */
+    generateNodePositions(node) {
+        let xOffset = 0;
+        let yOffset = 0;
+        // Update the offset if the node has a parent
+        if (node.prev_sources.length !== 0) {
+            const prevId = node.prev_sources[0];
+            const prevNode = this.state.sources.find(x => x.id === prevId);
+            // Check if the previous node has defined coordinates
+            if (prevNode.x_position !== null && prevNode.y_position !== null) {
+                xOffset = prevNode.x_position;
+                yOffset = prevNode.y_position;
+            }
+        }
+        // Helper variable to generate random positions
+        const rangeLimit = 300; // To generate positions in the interval [-rangeLimit, rangeLimit]
+        // Generate random positions
+        const xRandom = Math.floor(Math.random() * 2 * rangeLimit - rangeLimit);
+        const yRandom = Math.floor(Math.random() * 2 * rangeLimit - rangeLimit);
+
+        // Return positions with offset
+        return [xRandom + xOffset, yRandom + yOffset];
+    }
+
+    // Helper function to setup the nodes and edges for the graph
+    createNodesAndEdges() {
+        let nodes = new DataSet();
+        let edges = new DataSet();
+        // Iterate through each node in the graph and build the arrays of nodes and edges
+        for (let index in this.state.sources) {
+            let node = this.state.sources[index];
+            // Deal with positions
+            if (node.x_position === null || node.y_position === null) {
+                // If position is still undefined, generate random x and y in interval [-300, 300]
+                const [x, y] = this.generateNodePositions(node);
+                this.updateSourcePosition(node.id, x, y);
+                nodes.add({id: node.id, label: node.title, x: x, y: y});
+            } else {
+                nodes.add({id: node.id, label: node.title, x: node.x_position, y: node.y_position});
+            }
+            // Deal with edges
+            for (let nextIndex in node.next_sources) {
+                const nextId = node.next_sources[nextIndex];
+                edges.add({from: node.id, to: nextId})
+            }
+        }
+        this.setState({visNodes: nodes, visEdges: edges});
+        return [nodes, edges];
+    }
+
+    renderNetwork = () => {
+        if (this.props.curProject === null) return;
+
+        this.getSources(() => {
+            const [nodes, edges] = this.createNodesAndEdges();
+
+            // create a network
+            const container = document.getElementById('mindmap');
+
+            // provide the data in the vis format
+            const data = {
+                nodes: nodes,
+                edges: edges
+            };
+            const options = {
+                nodes: {
+                    shape: "box",
+                    size: 16,
+                    margin: 10,
+                    physics: false,
+                    chosen: false,
+                    font: {
+                        face: "Apple-System",
+                        color: "white"
+                    },
+                    color: {
+                        background: getComputedStyle(document.querySelector(".rs-btn-primary"))["background-color"]
+                    },
+                    widthConstraint: {
+                        maximum: 500
                     }
                 },
-                color: "black",
-                physics: false,
-                smooth: false,
-                hoverWidth: 0
-            },
-            interaction: {
-                navigationButtons: true,
-                selectConnectedEdges: false,
-                hover: true,
-                hoverConnectedEdges: false
-            },
-            manipulation: {
-                enabled: true,
-                deleteNode: false,
-                // addNode: this.addNode,
-                // deleteEdge: this.deleteEdge,
-                // addEdge: this.addEdge,
-                editEdge: false
-            }
-        };
+                edges: {
+                    arrows: {
+                        to: {
+                            enabled: true
+                        }
+                    },
+                    color: "black",
+                    physics: false,
+                    smooth: false,
+                    hoverWidth: 0
+                },
+                interaction: {
+                    navigationButtons: true,
+                    selectConnectedEdges: false,
+                    hover: true,
+                    hoverConnectedEdges: false
+                },
+                manipulation: {
+                    enabled: true,
+                    deleteNode: false,
+                    // addNode: this.addNode,
+                    // deleteEdge: this.deleteEdge,
+                    // addEdge: this.addEdge,
+                    editEdge: false
+                }
+            };
 
-        // initialize your network!
-        const network = new Network(container, data, options);
-        network.fit()
+            // Initialize the network
+            const network = new Network(container, data, options);
+            network.fit()
 
-        // Handle click vs drag
-        network.on("click", (params) => {
-            if (params.nodes !== undefined && params.nodes.length > 0) {
-                const nodeId = params.nodes[0];
-                this.handleClickedNode(nodeId);
-            }
-        });
+            // Handle click vs drag
+            network.on("click", (params) => {
+                if (params.nodes !== undefined && params.nodes.length > 0) {
+                    const nodeId = params.nodes[0];
+                    this.handleClickedNode(nodeId);
+                }
+            });
 
-        // Set cursor to pointer when hovering over a node
-        network.on("hoverNode", () => network.canvas.body.container.style.cursor = "pointer");
-        network.on("blurNode", () => network.canvas.body.container.style.cursor = "default");
+            // Update positions after dragging node
+            network.on("dragEnd", () => {
+                // Only update positions if there is a selected node
+                if (network.getSelectedNodes().length !== 0) {
+                    const id = network.getSelectedNodes()[0];
+                    const position = network.getPosition(id);
+                    const x = position.x;
+                    const y = position.y;
+                    this.updateSourcePosition(id, x, y);
+                }
+            });
 
-        // Store the network
-        this.setState({network: network});
+            // Set cursor to pointer when hovering over a node
+            network.on("hoverNode", () => network.canvas.body.container.style.cursor = "pointer");
+            network.on("blurNode", () => network.canvas.body.container.style.cursor = "default");
+
+            // Store the network
+            this.setState({network: network});
+        })
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.curProject !== this.props.curProject) {
+            this.renderNetwork();
+        }
+    }
+
+    componentDidMount() {
+        this.renderNetwork();
     }
 
     render() {
+        if (this.props.curProject === null) return <Placeholder.Paragraph rows={20} active/>
+
         return (
             <div>
                 <div id="mindmap"/>
@@ -233,7 +380,13 @@ function AppHeader(props) {
                     <img className="limit-height" src={horizontalLogo} alt="Knolist"/>
                 </Navbar.Header>
                 <FlexboxGrid.Item>
-                    <span id="project-title">Current Project: {props.curProject}</span>
+                    <span id="project-title">
+                        {
+                            props.curProject === null ?
+                                null :
+                                "Current Project: " + props.curProject.title
+                        }
+                    </span>
                 </FlexboxGrid.Item>
                 <FlexboxGrid.Item>
                     <SearchAndFilter/>
@@ -323,30 +476,64 @@ function SearchBar() {
     );
 }
 
-function ProjectsSidebar(props) {
-    const projects = ["Knolist", "My Last Duchess", "Grad School Application"];
-    return (
-        <Drawer
-            size="xs"
-            show={props.show}
-            onHide={props.close}>
-            <Drawer.Header>
-                <Drawer.Title>Your Projects</Drawer.Title>
-            </Drawer.Header>
-            <Drawer.Body style={{marginBottom: 10}}>
-                <ProjectsList projects={projects} curProject={props.curProject} setCurProject={props.setCurProject}/>
-            </Drawer.Body>
-            <Drawer.Footer>
-                <IconButton appearance="primary" icon={<Icon icon="plus"/>} circle size="lg"/>
-            </Drawer.Footer>
-        </Drawer>
-    )
+class ProjectsSidebar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            projects: null
+        }
+    }
+
+    getProjects = async () => {
+        const url = this.props.baseUrl + "/projects"
+        const response = await fetch(url, {
+            headers: {
+                "Authorization": "Bearer " + this.props.jwt
+            }
+        }).then(response => response.json());
+        if (response.success) {
+            this.setState({projects: response.projects})
+        } else {
+            alert("Something went wrong!")
+        }
+    }
+
+    renderProjectsList = () => {
+        if (this.state.projects === null) return <Placeholder.Paragraph rows={15} active/>;
+
+        return <ProjectsList projects={this.state.projects} curProject={this.props.curProject}
+                             setCurProject={this.props.setCurProject}/>
+    }
+
+    componentDidMount() {
+        this.getProjects();
+    }
+
+    render() {
+        return (
+            <Drawer
+                size="xs"
+                show={this.props.show}
+                onHide={this.props.close}>
+                <Drawer.Header>
+                    <Drawer.Title>Your Projects</Drawer.Title>
+                </Drawer.Header>
+                <Drawer.Body style={{marginBottom: 10}}>
+                    {this.renderProjectsList()}
+                </Drawer.Body>
+                <Drawer.Footer>
+                    <IconButton appearance="primary" icon={<Icon icon="plus"/>} circle size="lg"/>
+                </Drawer.Footer>
+            </Drawer>
+        )
+    }
 }
 
 function ProjectsList(props) {
     return (
-        <Nav vertical activeKey={props.curProject} onSelect={(eventKey) => props.setCurProject(eventKey)}>
-            {props.projects.map(project => <Project key={project} project={project} eventKey={project}/>)}
+        <Nav vertical activeKey={props.curProject === null ? undefined : props.curProject.id}
+             onSelect={(eventKey) => props.setCurProject(eventKey)}>
+            {props.projects.map(project => <Project key={project.id} project={project} eventKey={project.id}/>)}
         </Nav>
     );
 }
@@ -356,7 +543,7 @@ function Project(props) {
         <Nav.Item {...props}>
             <FlexboxGrid justify="space-between">
                 <FlexboxGrid.Item>
-                    <Icon icon={"project"}/> {props.project}
+                    <Icon icon={"project"}/> {props.project.title}
                 </FlexboxGrid.Item>
                 <FlexboxGrid.Item>
                     <ButtonToolbar>
