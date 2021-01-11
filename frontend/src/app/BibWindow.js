d73a9ee8f06a52a5947184bb78f62c3e18d91f67
renaaliu <renaliu92@gmail.com>
2021-01-11 11:14:15 -0500

    CU-3153c2 Export Bibliography Button

--- frontend/src/app/BibWindow.js
+++ frontend/src/app/BibWindow.js
@@ -3,8 +3,6 @@ import {
     Modal
 } from "rsuite";
 
-import makeHttpRequest from "../services/HttpRequest";
-
 class BibWindow extends React.Component {
     constructor(props) {
         super(props);
@@ -14,16 +12,7 @@ class BibWindow extends React.Component {
         }
     }
 
-    // getCitation = async () => {
-    //     // const endpoint = "/sources/" + this.props.selectedNode;
-    //     const endpoint = "/sources/" + this.randomNode;
-    //     const response = await makeHttpRequest(endpoint);
-    //     this.setState({sourceInfo: response.body.source});
-    // }
-    // 
-
     render() {
-        const source = this.state.sourceInfo;
         if (this.props.sources === null) return null;
         return (
             <Modal full show={this.props.showBib} onHide={() => this.props.setShowBib(false)}>
@@ -43,47 +32,5 @@ class BibWindow extends React.Component {
         );
     }
 }
-
-// class Citation extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             newSourceTitleInputId: "new-source-title-input"
-//         }
-//     }
-
-//     updateTitle = (callback) => {
-//         const newTitle = document.getElementById(this.state.newSourceTitleInputId).value;
-//         if (newTitle === this.props.source.title) {
-//             callback();
-//             return;
-//         }
-//         const endpoint = "/sources/" + this.props.source.id;
-//         const body = {
-//             "title": newTitle
-//         }
-
-//         makeHttpRequest(endpoint, "PATCH", body).then(() => {
-//             this.props.renderNetwork(() => {
-//                 this.props.getSourceDetails().then(() => {
-//                     this.setLoading(false);
-//                     callback();
-//                 });
-//             });
-//         });
-//     }
-
-//     renderTitle = () => {
-//         return <a target="_blank" rel="noopener noreferrer" style={{marginRight: 10}}>href={this.props.source.url}</a>
-//     }
-
-//     render() {
-//         return (
-//             <div style={{display: "flex"}}>
-//                 {this.renderTitle()}
-//             </div>
-//         );
-//     }
-// }
 
 export default BibWindow;
