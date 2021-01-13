f1f8e049beff1b270cbb458cb97b858b4f86b00e
renaaliu <renaliu92@gmail.com>
2021-01-12 21:21:32 -0500

    CU-3333dh

--- frontend/src/app/BibWindow.js
+++ frontend/src/app/BibWindow.js
@@ -13,7 +13,7 @@ class BibWindow extends React.Component {
             CHI: "Chicago"
         }
         this.state = {
-            formatType: formats.APA,
+            curFormat: formats.APA,
             copyBib: false,
             formats: formats
         }
@@ -28,7 +28,7 @@ class BibWindow extends React.Component {
 
     changeFormatType = (type) => {
         this.setState({
-    //         formatType: type 
+            curFormat: type 
         });
     }
 
@@ -41,8 +41,12 @@ class BibWindow extends React.Component {
     }
 
     renderFormatType = (source) => {
-        if (this.state.formatType === this.state.formats.APA){
+        if (this.state.curFormat === this.state.formats.APA){
             return <p>source.author. (source.publishDate). "{source.title}." <i>source.siteName</i>, {source.url}.</p> 
+        } else if (this.state.curFormat === this.state.formats.CHI){
+            return <p>source.author. "{source.title}." <i>source.siteName</i>, source.publishDate. source.accessDate. {source.url}.</p>
+        } else if (this.state.curFormat === this.state.formats.MLA) {
+            return <p>source.author. "{source.title}." <i>source.siteName</i>, source.publishDate, {source.url}. Accessed source.accessDate. </p>
         }
     }
 
@@ -55,7 +59,7 @@ class BibWindow extends React.Component {
                     <Modal.Title>
                     Bibliography
                     </Modal.Title>
-                    <SelectPicker data={formats}/>
+                    <SelectPicker data={formats} onChange={this.changeFormatType}/>
                     {/* TODO: Set copyBib state to "export"
                     <IconButton onClick={() => this.state.copyBib(true)} icon={<Icon icon="copy"/>}/>
                     */}
@@ -66,7 +70,7 @@ class BibWindow extends React.Component {
                         <p>Included</p>
                         {this.props.sources.map((source,index) => 
                             <Checkbox defaultChecked onChange={this.removeFromSaved} key={index}>
-                                {this.renderFormatType({source})}
+                                {this.renderFormatType(source)}
                                 <Whisper placement="bottomStart" trigger="hover"  speaker={<Tooltip>This source is missing a field</Tooltip>}>
                                     <Icon icon="exclamation-circle" style={{ color: '#f5a623' }}/>
                                 </Whisper>
@@ -76,33 +80,6 @@ class BibWindow extends React.Component {
                 </Modal.Body>
             </Modal>
         );
-
-        // return (
-        //     <Modal full show={this.props.showBib} onHide={() => this.props.setShowBib(false)}>
-        //         <Modal.Header>
-        //             <Modal.Title>
-        //             Bibliography
-        //             </Modal.Title>
-        //             <Dropdown title={this.state.formatType} activeKey={this.state.formatType}>
-        //                 <Dropdown.Item eventKey="APA">APA</Dropdown.Item>
-        //                 <Dropdown.Item eventKey="MLA">MLA</Dropdown.Item>
-        //                 <Dropdown.Item eventKey="CHI">Chicago</Dropdown.Item>
-        //             </Dropdown>
-        //             <IconButton onClick={() => this.state.copyBib(true)} icon={<Icon icon="copy"/>}/>
-        //         </Modal.Header>
-        //         <Modal.Body>
-        //             <ul>
-        //                 {if (this.state.formatType === "MLA")
-        //                     {this.props.sources.map((source,index) => <li key={index}>source.author. "{source.title}." <i>source.siteName</i>, source.publishDate, {source.url}. Accessed source.accessDate. </li>)}
-        //                 else if (this.state.formatType === "CHI")
-        //                     {this.props.sources.map((source,index) => <li key={index}>source.author. "{source.title}." <i>source.siteName</i>, source.publishDate. source.accessDate. {source.url}. </li>)}
-        //                 else if (this.state.formatType === "APA")
-        //                     {this.props.sources.map((source,index) => <li key={index}>source.author. (source.publishDate). "{source.title}." <i>source.siteName</i>, {source.url}. </li>)}
-        //                 } 
-        //             </ul>
-        //         </Modal.Body>
-        //     </Modal>
-        // );
     }
 }
 
--- frontend/src/app/urlToCitation.py
+++ frontend/src/app/urlToCitation.py
@@ -3,87 +3,84 @@ from bs4 import BeautifulSoup
 from datetime import date
 
 # Takes list of string urls
-def urlToCitation(sites):
+def urlToCitation(url):
     today = date.today()
-    citationPerSource = dict()
-    for url in sites:
-        citationFields = {
-            "title": "", 
-            "author": "", 
-            "publishDate": "", 
-            "siteName": "", 
-            # Set accessDate to time of creating source (future TODO: update with access history)
-            "accessDate": today.strftime("%m/%d/%Y"), 
-        }
+    citationFields = {
+        "title": "", 
+        "author": "", 
+        "publishDate": "", 
+        "siteName": "", 
+        # Set accessDate to time of creating source (future TODO: update with access history)
+        "accessDate": today.strftime("%m/%d/%Y"), 
+    }
 
-        r = requests.get(url)
-        html_source_string = r.text
-        soup = BeautifulSoup(html_source_string, 'html.parser')
+    r = requests.get(url)
+    html_source_string = r.text
+    soup = BeautifulSoup(html_source_string, 'html.parser')
 
-        # Only one version of each these fields should be 
-        # in a page source, but there is more than one way to denote each field
-        title1 = soup.title
-        title2 = soup.find("meta",  property="og:title")
-        title3 = soup.find("meta", name="dc.Title")
-        title4 = soup.find("meta", property="twitter:title")
+    # Only one version of each these fields should be 
+    # in a page source, but there is more than one way to denote each field
+    title1 = soup.title
+    title2 = soup.find("meta",  property="og:title")
+    title3 = soup.find("meta", name="dc.Title")
+    title4 = soup.find("meta", property="twitter:title")
 
-        author1 = soup.find("meta",  name="twitter:data1")
-        author2 = soup.find("meta",  name="dc.Creator")
-        author3 = soup.find("meta",  name="author")
+    author1 = soup.find("meta",  name="twitter:data1")
+    author2 = soup.find("meta",  name="dc.Creator")
+    author3 = soup.find("meta",  name="author")
 
-        # TODO: If Author name (not "Staff") split into First and Last to order
+    # TODO: If Author name (not "Staff") split into First and Last to order
 
-        publishDate1 = soup.find("meta",  property="article:published_time")
-        publishDate2 = soup.find("meta",  name="dc.Date")
+    publishDate1 = soup.find("meta",  property="article:published_time")
+    publishDate2 = soup.find("meta",  name="dc.Date")
 
-        siteName1 = soup.find("meta",  property="site_name")
-        siteName2 = soup.find("meta",  name="citation_journal_title")
+    siteName1 = soup.find("meta",  property="site_name")
+    siteName2 = soup.find("meta",  name="citation_journal_title")
 
-        # Populate field with whichever method does not return None
-        if title1:
-            citationFields["title"] = title1["content"]
-            break
-        else if title2:
-            citationFields["title"] = title2["content"]
-            break
-        else if title3:
-            citationFields["title"] = title3["content"]
-            break
-        else:
-            citationFields["title"] = "Not found"
+    # Populate field with whichever method does not return None
+    if title1:
+        citationFields["title"] = title1["content"]
+        break
+    else if title2:
+        citationFields["title"] = title2["content"]
+        break
+    else if title3:
+        citationFields["title"] = title3["content"]
+        break
+    else:
+        citationFields["title"] = "Not found"
 
-        if author1:
-            citationFields["author"] = author1["content"]
-            break
-        else if author2:
-            citationFields["author"] = author2["content"]
-            break
-        else if author3:
-            citationFields["author"] = author3["content"]
-            break
-        else:
-            citationFields["author"] = "Not found"
+    if author1:
+        citationFields["author"] = author1["content"]
+        break
+    else if author2:
+        citationFields["author"] = author2["content"]
+        break
+    else if author3:
+        citationFields["author"] = author3["content"]
+        break
+    else:
+        citationFields["author"] = "Not found"
 
-        if publishDate1:
-            citationFields["publishDate"] = publishDate1["content"]
-            break
-        else if publishDate2:
-            citationFields["publishDate"] = publishDate2["content"]
-            break
-        else:
-            citationFields["publishDate"] = "Not found"
-        
-        if siteName1:
-            citationFields["siteName"] = siteName1["content"]
-            break
-        else if siteName2:
-            citationFields["siteName"] = siteName2["content"]
-            break
-        else:
-            citationFields["siteName"] = "Not found"
-
-        citationPerSource[url] = citationFields
-    return citationPerSource
+    if publishDate1:
+        citationFields["publishDate"] = publishDate1["content"]
+        break
+    else if publishDate2:
+        citationFields["publishDate"] = publishDate2["content"]
+        break
+    else:
+        citationFields["publishDate"] = "Not found"
+    
+    if siteName1:
+        citationFields["siteName"] = siteName1["content"]
+        break
+    else if siteName2:
+        citationFields["siteName"] = siteName2["content"]
+        break
+    else:
+        citationFields["siteName"] = "Not found"
+    
+    return citationFields
 
 
 
