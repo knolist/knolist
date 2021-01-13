aae28dc870bf6b9ed2d1d2f29b9c5d9b1853abca
Andy Chen <andych3n@yahoo.com>
2021-01-12 22:30:48 -0500

    CU-3333hw
    Set up configuration files for authentication

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './app/App.js';
+import { BrowserRouter as Router } from 'react-router-dom';
+import KnolistAuthProvider from "./authentication/auth-provider";
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <KnolistAuthProvider>
+            <App/>
+        </KnolistAuthProvider>
+    </Router>, 
+    document.getElementById('root')
+);
