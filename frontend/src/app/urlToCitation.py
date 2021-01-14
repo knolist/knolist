4c9042ddca2460de020e6648c7f35c950365c47d
renaaliu <renaliu92@gmail.com>
2021-01-13 23:00:40 -0500

    CU-3333dr

--- frontend/src/app/urlToCitation.py
+++ frontend/src/app/urlToCitation.py
@@ -22,65 +22,55 @@ def urlToCitation(url):
     # in a page source, but there is more than one way to denote each field
     title1 = soup.title
     title2 = soup.find("meta",  property="og:title")
-    title3 = soup.find("meta", name="dc.Title")
+    title3 = soup.find(attrs={'name':'dc.Title'})
     title4 = soup.find("meta", property="twitter:title")
 
-    author1 = soup.find("meta",  name="twitter:data1")
-    author2 = soup.find("meta",  name="dc.Creator")
-    author3 = soup.find("meta",  name="author")
+    author1 = soup.find(attrs={'name':'twitter:data1'})
+    author2 = soup.find(attrs={'name':'dc.Creator'})
+    author3 = soup.find(attrs={'name':'author'})
 
     # TODO: If Author name (not "Staff") split into First and Last to order
 
     publishDate1 = soup.find("meta",  property="article:published_time")
-    publishDate2 = soup.find("meta",  name="dc.Date")
+    publishDate2 = soup.find(attrs={'name':'dc.Date'})
 
     siteName1 = soup.find("meta",  property="site_name")
-    siteName2 = soup.find("meta",  name="citation_journal_title")
+    siteName2 = soup.find(attrs={'name':'citation_journal_title'})
+    siteName3 = soup.find("meta",  property="og:site_name")
 
     # Populate field with whichever method does not return None
-    if title1:
-        citationFields["title"] = title1["content"]
-        break
-    else if title2:
+    if title2:
         citationFields["title"] = title2["content"]
-        break
-    else if title3:
+    elif title3:
         citationFields["title"] = title3["content"]
-        break
+    elif title1:
+        citationFields["title"] = title1.string
     else:
         citationFields["title"] = "Not found"
 
-    if author1:
-        citationFields["author"] = author1["content"]
-        break
-    else if author2:
+    if author2:
         citationFields["author"] = author2["content"]
-        break
-    else if author3:
+    elif author3:
         citationFields["author"] = author3["content"]
-        break
+    elif author1:
+        citationFields["author"] = author1["content"]
     else:
         citationFields["author"] = "Not found"
 
     if publishDate1:
         citationFields["publishDate"] = publishDate1["content"]
-        break
-    else if publishDate2:
+    elif publishDate2:
         citationFields["publishDate"] = publishDate2["content"]
-        break
     else:
         citationFields["publishDate"] = "Not found"
     
     if siteName1:
         citationFields["siteName"] = siteName1["content"]
-        break
-    else if siteName2:
+    elif siteName2:
         citationFields["siteName"] = siteName2["content"]
-        break
+    elif siteName3:
+        citationFields["siteName"] = siteName3["content"]
     else:
         citationFields["siteName"] = "Not found"
     
     return citationFields
-
-
-
