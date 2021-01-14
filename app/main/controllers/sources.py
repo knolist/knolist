58ac2afdc5d66c916d75100caac362df59a1ff39
noahpark <npark9@jhu.edu>
2021-01-14 14:40:09 -0500

    CU-3332q2  added a few more updated endpoints, still am not sure which endpoints I can delete

--- app/main/controllers/sources.py
+++ app/main/controllers/sources.py
@@ -1,6 +1,7 @@
 import json
 
 from flask import request, abort, jsonify
+from sqlalchemy import String, BOOLEAN
 
 from ..models.models import Project, Source, Item
 from ..auth import requires_auth, AuthError
@@ -18,6 +19,7 @@ def get_authorized_source(user_id, source_id):
         }, 403)
 
     return source
+
 
 def get_authorized_item(user_id, item_id):
     item = Item.query.get(item_id)
@@ -37,6 +39,7 @@ def set_source_routes(app):
     """
     Reads the detailed information of a specific source.
     """
+
     @app.route('/sources/<int:source_id>')
     @requires_auth('read:sources-detail')
     def get_source_detail(user_id, source_id):
@@ -47,13 +50,13 @@ def set_source_routes(app):
             'source': source.format_long()
         })
 
-
     """
     Reads the detailed information of a specific item.
     """
+
     @app.route('/items/<int:item_id>')
     @requires_auth('read:items-detail')
-    def get_item_detaiil(user_id, item_id):
+    def get_item_detail(user_id, item_id):
         item = get_authorized_item(user_id, item_id)
 
         return jsonify({
@@ -61,10 +64,10 @@ def set_source_routes(app):
             'item': item.format()
         })
 
-
     """
     Deletes a source.
     """
+
     @app.route('/sources/<int:source_id>', methods=['DELETE'])
     @requires_auth('delete:sources')
     def delete_source(user_id, source_id):
@@ -77,10 +80,10 @@ def set_source_routes(app):
             'deleted': source_id
         })
 
-
     """
     Deletes an item
     """
+
     @app.route('/items/<int:item_id>', methods=['DELETE'])
     @requires_auth('delete:items')
     def delete_item(user_id, item_id):
@@ -93,11 +96,11 @@ def set_source_routes(app):
             'deleted': item_id
         })
 
-
     """
     Updates information in a source.
     The information to be updated is passed in a JSON body.
     """
+
     @app.route('/sources/<int:source_id>', methods=['PATCH'])
     @requires_auth('update:sources')
     def update_source(user_id, source_id):
@@ -112,16 +115,13 @@ def set_source_routes(app):
         content = body.get('content', None)
         x_position = body.get('x_position', None)
         y_position = body.get('y_position', None)
-        # Obtain JSON list attributes
-        highlights = body.get('highlights', None)
-        notes = body.get('notes', None)
         # Obtain project ID
         project_id = body.get('project_id', None)
 
         # Verify that at least one parameter was passed
         cond_1 = title is None and content is None
         cond_2 = x_position is None and y_position is None
-        cond_3 = highlights is None and notes is None and project_id is None
+        cond_3 = project_id is None
         if cond_1 and cond_2 and cond_3:
             abort(400)
 
@@ -130,9 +130,7 @@ def set_source_routes(app):
             abort(422)
         if y_position is not None and type(y_position) is not int:
             abort(422)
-        if highlights is not None and type(highlights) is not list:
-            abort(422)
-        if notes is not None and type(notes) is not list:
+        if content is not None and type(content) is not String:
             abort(422)
         if project_id is not None:
             project = Project.query.get(project_id)
@@ -152,13 +150,10 @@ def set_source_routes(app):
         # Update values that are not None
         source.title = title if title is not None else source.title
         source.content = content if content is not None else source.content
-        source.x_position = x_position if x_position is not None\
+        source.x_position = x_position if x_position is not None \
             else source.x_position
         source.y_position = y_position if y_position is not None \
             else source.y_position
-        source.highlights = json.dumps(highlights) if highlights is not None \
-            else source.highlights
-        source.notes = json.dumps(notes) if notes is not None else source.notes
         source.project_id = project_id if project_id is not None \
             else source.project_id
 
@@ -169,10 +164,84 @@ def set_source_routes(app):
             'source': source.format_long()
         })
 
+
+    """
+    Updates the information in an item.
+    The information to be updated is passed in a JSON body.
+    """
+
+    @app.route('/item/<int:item_id>', methods=['PATCH'])
+    @requires_auth('update:items')
+    def update_item(user_id, item_id):
+        item = get_authorized_item(user_id, item_id)
+
+        body = request.get_json()
+        if body is None:
+            abort(400)
+
+        # Obtain the simple attributes
+        is_note = body.get('is_note', None)
+        is_highlight = body.get('is_highlight', None)
+        content = body.get('content', None)
+        x_position = body.get('x_position', None)
+        y_position = body.get('y_position', None)
+        # Obtain source id
+        source_id = body.get('source_id', None)
+        # Obtain project ID
+        project_id = body.get('project_id', None)
+
+        # Verify that at least one parameter was passed
+        cond_1 = is_note is None and is_highlight is None
+        cond_2 = content is None and x_position is None
+        cond_3 = y_position is None and source_id is None and project_id is None
+        if cond_1 and cond_2 and cond_3:
+            abort(400)
+
+        # Verify that parameters are correctly formatted
+        if x_position is not None and type(x_position) is not int:
+            abort(422)
+        if y_position is not None and type(y_position) is not int:
+            abort(422)
+        if content is not None and type(content) is not String:
+            abort(422)
+        if is_note is not None and type(is_note) is not BOOLEAN:
+            abort(422)
+        if is_highlight is not None and type(is_highlight) is not BOOLEAN:
+            abort(422)
+
+        if project_id is not None:
+            project = Project.query.get(project_id)
+            if project is None:
+                abort(422)
+            if project.user_id != user_id:
+                raise AuthError({
+                    'code': 'invalid_user',
+                    'description':
+                        'This item does not belong to the requesting user.'
+                }, 403)
+
+        # Update values that are not None
+        item.source_id = source_id if source_id is not None else item.source_id
+        item.is_note = is_note if is_note is not None else item.is_note
+        item.is_highlight = is_highlight if is_highlight is not None else item.is_highlight
+        item.content = content if content is not None else item.content
+        item.x_position = x_position if x_position is not None else item.x_position
+        item.y_position = y_position if y_position is not None else item.y_position
+        item.parent_project = project_id if project_id is not None else item.parent_project
+
+        item.update()
+
+        return jsonify({
+            'success': True,
+            'source': item.format()
+        })
+
+
     """
     Adds a new highlight to a source's highlights.
     The highlight is passed in a JSON body.
     """
+
     @app.route('/sources/<int:source_id>/highlights', methods=['POST'])
     @requires_auth('create:highlights')
     def add_highlights(user_id, source_id):
@@ -200,6 +269,7 @@ def set_source_routes(app):
     Deletes one or more highlights from a source.
     Highlights to be deleted are passed as a list of indices in a JSON body.
     """
+
     @app.route('/sources/<int:source_id>/highlights', methods=['DELETE'])
     @requires_auth('delete:highlights')
     def delete_highlights(user_id, source_id):
@@ -227,6 +297,7 @@ def set_source_routes(app):
     """
     Adds a new note to a source's notes. The note is passed in a JSON body.
     """
+
     @app.route('/sources/<int:source_id>/notes', methods=['POST'])
     @requires_auth('create:notes')
     def add_notes(user_id, source_id):
@@ -254,6 +325,7 @@ def set_source_routes(app):
     Deletes one or more notes from a source.
     Notes to be deleted are passed as a list of indices in a JSON body.
     """
+
     @app.route('/sources/<int:source_id>/notes', methods=['DELETE'])
     @requires_auth('delete:notes')
     def delete_notes(user_id, source_id):
@@ -282,6 +354,7 @@ def set_source_routes(app):
     Updates the content of one of a source's notes.
     The note ID and new content are passed in a JSON body.
     """
+
     @app.route('/sources/<int:source_id>/notes', methods=['PATCH'])
     @requires_auth('update:notes')
     def update_note(user_id, source_id):
