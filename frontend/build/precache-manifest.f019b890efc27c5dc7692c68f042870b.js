c6dac5baaa79fb8fcb38aeee5529cd993167ddf5
Vinicius Lepca <vinicius.lepca@gmail.com>
2021-02-27 14:29:16 -0300

    Heroku deployment

---
+++ frontend/build/precache-manifest.f019b890efc27c5dc7692c68f042870b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a5496c88c647244e52ae5b750c144e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a21ba4d5859a5dbf59cb",
+    "url": "/static/css/2.c37a399a.chunk.css"
+  },
+  {
+    "revision": "5bb9fddb956fb8790fcb",
+    "url": "/static/css/main.e57ab2a4.chunk.css"
+  },
+  {
+    "revision": "a21ba4d5859a5dbf59cb",
+    "url": "/static/js/2.d99395f1.chunk.js"
+  },
+  {
+    "revision": "84cb4889b7c40a5d2de307ce5f3ec8e7",
+    "url": "/static/js/2.d99395f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bb9fddb956fb8790fcb",
+    "url": "/static/js/main.53eb4e6e.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "7429f5ff7e41d7e317b7eb677882d687",
+    "url": "/static/media/horizontal_main.7429f5ff.png"
+  }
+]);
