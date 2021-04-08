a7131c5edcc51066053e47190b57b7de6610692c
Vinicius Lepca <vinicius.lepca@gmail.com>
2021-04-08 15:31:19 -0300

    New build

---
+++ frontend/build/precache-manifest.86107ecedb6586a223d553a37cffa794.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d96e2ff915babbab9bed406bab6020",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23db0061f93ab625f4f9",
+    "url": "/static/css/2.c37a399a.chunk.css"
+  },
+  {
+    "revision": "9e438fb454717b7b281d",
+    "url": "/static/css/main.e57ab2a4.chunk.css"
+  },
+  {
+    "revision": "23db0061f93ab625f4f9",
+    "url": "/static/js/2.a76168a4.chunk.js"
+  },
+  {
+    "revision": "84cb4889b7c40a5d2de307ce5f3ec8e7",
+    "url": "/static/js/2.a76168a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e438fb454717b7b281d",
+    "url": "/static/js/main.073278a4.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "7429f5ff7e41d7e317b7eb677882d687",
+    "url": "/static/media/horizontal_main.7429f5ff.png"
+  }
+]);
