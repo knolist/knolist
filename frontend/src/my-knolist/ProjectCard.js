d090d578279ee57a2f708dc2616746288862a190
cldelahan <cldelahan@gmail.com>
2021-04-09 03:31:41 -0400

    add frontend visualization for projet stats CU-3t1tkw

--- frontend/src/my-knolist/ProjectCard.js
+++ frontend/src/my-knolist/ProjectCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Panel, Icon } from "rsuite";
 import { Link } from "react-router-dom";
+import makeHttpRequest from "../services/HttpRequest.js";
 
 function ProjectCard(props) {
   const openProject = (project) => {
@@ -8,35 +9,68 @@ function ProjectCard(props) {
     //set current project to selected project
   }
 
+
   let description = "";
-  if (props.data.description != null) description = props.data.description;
+    if (props.data.description != null) description = <p> {props.data.description} </p>;
+
+  let stats = [];
+  if (props.stats !== undefined) {
+      if (props.stats['num_items'] !== undefined) {
+          stats.push(<p key={'num_items'}> {props.stats['num_items'].toString()}  total items </p>)
+      }
+      if (props.stats.average_depth_per_item !== undefined) {
+          stats.push(<p key={'avg_depth'}> Average depth of {props.stats.average_depth_per_item} </p>)
+      }
+      if (props.stats.date_accessed !== undefined) {
+          stats.push(<p key={'date_accessed'}> Last edit was {props.stats.date_accessed}</p>)
+      }
+      if (props.stats.date_created !== undefined) {
+          stats.push(<p key={'date_created'}> Project created on {props.stats.date_created}</p>)
+      }
+      if (props.stats.max_depth !== undefined) {
+          stats.push(<p key={'max_depth'}> Max depth of {props.stats.max_depth}</p>)
+      }
+      if (props.stats.most_common !== undefined) {
+          stats.push(<p key={'most_common'}> Most common item is {props.stats.most_common}</p>)
+      }
+  }
+
 
   if (props.data.shared_users.length > 0) {
     return (
-      <Link to="/" style={{ textDecoration: "none" }} className="react-router-styling">
+      <Link to="/" style={{ textDecoration: "none", paddingRight: "5vh" }} className="react-router-styling">
         <Panel
           shaded bordered
           header={props.data.title}
-          style={{ width: "17vw", marginTop: "2vh" }}
+          style={{marginTop: "2vh" }}
           onClick={() => openProject(props.data)}>
           {props.data.id}
           <Icon icon="people-group" style={{ float: "right", marginTop: "5px" }}/>
+
         </Panel>
       </Link>
     );
   } else {
     return (
-      <Link to="/" style={{ textDecoration: "none" }} className="react-router-styling">
         <Panel
-          shaded bordered
-          header={props.data.title}
-          style={{ width: "17vw", marginTop: "2vh" }}
-          onClick={() => openProject(props.data)}>
-          {description}
+            shaded bordered
+            header={
+                <Link to="/" onEnter={() => openProject(props.data)}
+                            style={{ textDecoration: "none", paddingRight: "5vh" }} className="react-router-styling">
+                {props.data.title}
+                </Link>
+            }
+            style={{marginTop: "2vh" }}
+            onClick={() => openProject(props.data)}
+            collapsible>
+            {description ? <> <b>Description</b> {description} <br/> </>: null}
+            <b> Statistics </b>
+            {stats}
         </Panel>
-      </Link>
+
     );
   }
 }
+
 
 export default ProjectCard;
